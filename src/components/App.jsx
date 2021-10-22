856b08e50f4bb1c7b0241e7106bda8f973d8ae87
ChidponJaimun <61172207+ChidponJaimun@users.noreply.github.com>
2021-10-22 17:37:21 +0700

    1

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>My Contacts</h1> <h2>Beyonce</h2>
+      <img
+        src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        alt="avatar_img"
+      />{" "}
+    </div>
+  );
+}
+
+export default App;
